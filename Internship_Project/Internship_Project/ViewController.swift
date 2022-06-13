703e6eacb60e46ab1b678c599f8008cae07476eb
VerdyanArman <107179710+VerdyanArman@users.noreply.github.com>
2022-06-13 18:38:03 +0400

    initial

---
+++ Internship_Project/Internship_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Internship_Project
+//
+//  Created by Cronos on 6/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
